c95ea9517980d596d5f9443f67005f8956eb0e48
Matthew Xia <mxia206@student.fuhsd.org>
2025-05-28 17:37:56 -0700

    tweak

--- src/main/java/tumtumtumsahur/Projectiles/ClusterFireball.java
+++ src/main/java/tumtumtumsahur/Projectiles/ClusterFireball.java
@@ -5,11 +5,11 @@ import tumtumtumsahur.Projectile;
 public class ClusterFireball extends Projectile{
 
     public ClusterFireball(String id, double x_pos, double y_pos, double dir, String pl_id) {
-        super(id, x_pos, y_pos, 40.0*Math.cos(dir), 40.0*Math.sin(dir), 40.0, pl_id);
+        super(id, x_pos, y_pos, 30.0*Math.cos(dir), 30.0*Math.sin(dir), 30.0, pl_id);
         this.time = 10;
         this.radius = 30.0;
         this.damage = 50.0;
-        this.stun_time = 15;
+        this.stun_time = 7;
         this.type = "clusterfireball";
     }
 }
