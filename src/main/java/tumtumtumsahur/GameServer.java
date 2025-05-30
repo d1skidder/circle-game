9f4736a3b25fb289f49a00fcea467bbd58f98f7a
15rv15 <rvenugopal086@student.fuhsd.org>
2025-05-30 01:39:25 -0700

    Update GameServer.java fix shi

--- src/main/java/tumtumtumsahur/GameServer.java
+++ src/main/java/tumtumtumsahur/GameServer.java
@@ -53,7 +53,7 @@ public class GameServer extends WebSocketServer {
     }
     private void handleChat(WebSocket ws, JsonNode jsonNode) {
         String message = jsonNode.has("message") ? jsonNode.get("message").asText() : null;
-        double timeT = jsonNode.has("time") ? jsonNode.get("time").isNull() : null;
+        double timeT = jsonNode.get("time").asDouble();
         Player pl = players.get(ws);
         pl.chat = message;
         pl.timeFromLastChat = timeT;
