f2c2ae8dff15e90c691f9571ca62d959969611e3
Matthew Xia <mxia206@student.fuhsd.org>
2025-05-31 22:02:34 -0700

    Fire class proj speed buff

--- src/main/java/tumtumtumsahur/Projectiles/ChonkyFireball.java
+++ src/main/java/tumtumtumsahur/Projectiles/ChonkyFireball.java
@@ -7,7 +7,7 @@ import tumtumtumsahur.*;
 public class ChonkyFireball extends Projectile {
 
     public ChonkyFireball(String id, double x_pos, double y_pos, double dir, Player pl) {
-        super(id, x_pos, y_pos, 50.0*Math.cos(dir), 50.0*Math.sin(dir), 50.0, pl);
+        super(id, x_pos, y_pos, 70.0*Math.cos(dir), 70.0*Math.sin(dir), 70.0, pl);
         this.time = 50;
         this.radius = 20.0;
         this.damage = 40.0;
--- src/main/java/tumtumtumsahur/Projectiles/Fireball.java
+++ src/main/java/tumtumtumsahur/Projectiles/Fireball.java
@@ -8,7 +8,7 @@ import tumtumtumsahur.*;
 public class Fireball extends Projectile {
 
     public Fireball(String id, double x_pos, double y_pos, double dir, Player pl) {
-        super(id, x_pos, y_pos, 60.0*Math.cos(dir), 60.0*Math.sin(dir), 60.0, pl);
+        super(id, x_pos, y_pos, 80.0*Math.cos(dir), 80.0*Math.sin(dir), 80.0, pl);
         this.time = 50;
         this.radius = 5.0;
         this.damage = 20.0;
